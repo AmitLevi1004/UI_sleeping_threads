974ea6a0ab13678dc3b6a75754e9ac87a86d412a
AmitLevi1004 <47784073+AmitLevi1004@users.noreply.github.com>
2019-02-19 10:56:34 -0800

    Add files via upload

---
+++ MyThread.java
@@ -0,0 +1,31 @@
+package q2_2012_b_94;
+
+import javax.swing.JButton;
+
+public class MyThread extends Thread{
+	JButton b;
+	
+	public MyThread(JButton b) {
+		this.b=b;
+		
+		
+	}
+	
+	public void run() {
+		int num=0;
+		int count = (int)(Math.random()*100+1);
+		try {
+			num = Integer.parseInt(b.getText());
+		}
+		catch(NumberFormatException e) {}
+			for(int i=1; i<=count;i++) {
+				b.setText(""+(num+1)%10);
+			try {
+				Thread.sleep(100);
+			}
+			catch(InterruptedException e) {}
+		}
+		
+	}
+
+}
